160f93d671b0214cc6d36d80ba24452a506f386e
unknown <talyamalka2000@gmail.com>
2024-11-12 18:59:29 +0200

    added stack

---
+++ DataStructure/stack.cpp
@@ -0,0 +1,28 @@
+#include "stack.h"
+
+void push(Stack* s, unsigned int element)
+{
+}
+
+int pop(Stack* s)
+{
+	return 0;
+}
+
+void initStack(Stack* s)
+{
+}
+
+void cleanStack(Stack* s)
+{
+}
+
+bool isEmpty(Stack* s)
+{
+	return false;
+}
+
+bool isFull(Stack* s)
+{
+	return false;
+}
