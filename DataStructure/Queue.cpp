6d727106d02af3302186cfe873390e0da02cd220
unknown <talyamalka2000@gmail.com>
2024-11-12 17:19:15 +0200

    header and code files added

---
+++ DataStructure/Queue.cpp
@@ -0,0 +1,31 @@
+#include "Queue.h"
+
+void initQueue(Queue* q, unsigned int size)
+{
+
+}
+
+void cleanQueue(Queue* q)
+{
+
+}
+
+void enqueue(Queue* q, unsigned int newValue)
+{
+
+}
+
+bool isEmpty(Queue* s)
+{
+
+}
+
+bool isFull(Queue* s)
+{
+
+}
+
+int dequeue(Queue* q)
+{
+
+}
